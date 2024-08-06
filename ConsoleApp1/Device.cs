c441a7ccefc260d57e3652634b754f331444a954
Andy <andyivanov2007@gmail.com>
2024-08-07 00:28:25 +0300

    Add project files.

---
+++ ConsoleApp1/Device.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Device
+    {
+        public string DevName { get; set; }
+        public string DevManuf { get; set; }
+        public double Price { get; set; }
+        public Device() { }
+        public Device(string devName, string devManuf, double price)
+        {
+            DevName = devName;
+            DevManuf = devManuf;
+            Price = price;
+
+        }
+        public override string ToString()
+        {
+            return $"Device name: {DevName}\nDevice manufacturer: {DevManuf}\nDevice price: {Price}";
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Создайте пользовательский тип "Устройство", который будет хранить следующую информацию
+информацию:
+ название устройства;
+ производитель устройства;
+ стоимость.
+Для двух массивов устройств реализуйте операции:
+ разность массивов;
+ пересечение массивов;
+ объединение массивов.
+Критерий для проведения операций - производитель устройства.
+*/
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Device> devices1 = new List<Device>() {
+               new Device("iPhone 13 Pro", "Apple",1000.0),
+               new Device("PlayStation 5", "Sony",699.0),
+               new Device("Xiaomi Mi Band 6", "Xiaomi",150.0),
+               new Device("Galaxy S21 Ultra", "Samsung", 950.0)
+           };
+            List<Device> devices2 = new List<Device>() {
+               new Device("MacBook Air M1", "Apple",1100.0),
+               new Device("Fitbit Charge 4", "Fitbit",210.0),
+               new Device("Kindle Paperwhite", "Amazon",180.0),
+               new Device("Nintendo Switch", "Nintendo", 320.0)
+           };
+            var diffArr = devices1.Where(dev1 => !devices2.Any(dev2 => dev2.DevManuf == dev1.DevManuf));
+            foreach (Device dev in diffArr) { Console.WriteLine(dev); }
+
+            Console.WriteLine();
+            var intersArr = devices1.Where(dev1 => devices2.Any(dev2 => dev2.DevManuf == dev1.DevManuf));
+            foreach (Device dev in intersArr) { Console.WriteLine(dev); }
+
+            Console.WriteLine();
+            var combArr = devices1.Union(devices2);
+            foreach (Device dev in combArr) { Console.WriteLine(dev); }
+        }
+    }
+}
